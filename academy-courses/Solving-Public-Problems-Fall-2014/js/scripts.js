b962371ae5cc526edb6a4e30fd91c160eaf33f81
luisdaniel <luisdaniel@gmail.com>
2014-08-12 17:04:48 -0400

    new directories

---
+++ academy-courses/Solving-Public-Problems-Fall-2014/js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+// Smooth Scrolling Function
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+			|| location.hostname == this.hostname) {
+
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+			$('html,body').animate({
+			  scrollTop: target.offset().top
+			}, 1000);
+			return false;
+			}
+		}
+	});
+});
+
+
+// Collapse for Readings
+
+$('.e-trigger').click(function() {
+	$(this).parent().toggleClass('m-active');
+})
+
+
+
+
+});
